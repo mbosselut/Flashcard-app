81974a79e9e8544572c2af67b9b47a56758b7ad2
mbosselut <duchmol7@hotmail.com>
2019-10-10 11:25:43 +0200

    Added random card button

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ let arrayOfQuestions = [
     }
 ]
 
+//TODO : Create function for initialising on page load
+
 // Turning a question into a flashcard 
 function createCard(cardObject) {
     const board = document.getElementById('board');
@@ -44,17 +46,39 @@ function loadCards(cardsArray){
         createCard(card);
     }
 }
-loadCards(arrayOfQuestions);
+// loadCards(arrayOfQuestions);
 
-// Function deleting a card
+// Deleting a card
 
 // Adding event listeners to the cards
-const cards = document.getElementsByClassName('flashcard');
-for (const card of cards) {
-    card.addEventListener('click', function(event) {
-        const children = card.childNodes;
-        console.log(children);
-        const arrChildren = Array.from(children);
-        arrChildren[1].classList.toggle('show');
-    })
+function addEventListeners(){
+    const cards = document.getElementsByClassName('flashcard');
+    for (const card of cards) {
+        card.addEventListener('click', function(event) {
+            const children = card.childNodes;
+            const arrChildren = Array.from(children);
+            arrChildren[1].classList.toggle('show');
+        })
+    }
+}
+addEventListeners();
+
+// Displaying a random card
+function randomCard(){
+    const randomIndex = Math.floor(Math.random() * arrayOfQuestions.length);
+    clearBoard();
+    createCard(arrayOfQuestions[randomIndex]);
+    addEventListeners();
+}
+
+// Event listener for button
+let button = document.querySelector("button");
+button.addEventListener("click", randomCard);
+
+// Clearing board
+function clearBoard(){
+    const board = document.getElementById('board');
+    while (board.firstChild) {
+        board.removeChild(board.firstChild);
+    };
 }
